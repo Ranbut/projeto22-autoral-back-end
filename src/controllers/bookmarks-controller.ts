879af1c6fa33c192e74a144fd5301fd4e4136da5
Vitor Gabriel <Ranbut_Beater@protonmail.com>
2023-06-15 12:29:51 -0300

    feat: Added history and updated bookmarks code

--- src/controllers/bookmarks-controller.ts
+++ src/controllers/bookmarks-controller.ts
@@ -2,7 +2,7 @@ import { Response, NextFunction } from 'express';
 import httpStatus from 'http-status';
 import bookmarksService from '@/services/bookmarks-service';
 import { AuthenticatedRequest } from '@/middlewares';
-import { TypeBookmark } from '@prisma/client';
+import { TypeInfo } from '@prisma/client';
 
 export async function getBookmark(req: AuthenticatedRequest, res: Response, next: NextFunction) {
     const { userId } = req;
@@ -32,7 +32,7 @@ export async function getBookmarks(req: AuthenticatedRequest, res: Response, nex
     const { type } = req.params;
 
     try {
-        const bookmarkType = type.toUpperCase() as TypeBookmark;
+        const bookmarkType = type.toUpperCase() as TypeInfo;
         const bookmark = await bookmarksService.getBookmarks(userId, bookmarkType);
         return res.status(httpStatus.OK).send(bookmark);
     } catch (error) {
@@ -49,7 +49,6 @@ export async function addBookmark(req: AuthenticatedRequest, res: Response, next
         await bookmarksService.addBookmark(userId, type, bookmarkData);
         return res.sendStatus(httpStatus.CREATED);
     } catch (error) {
-        console.log(error);
         next(error);
     }
 }
