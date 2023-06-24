f29c9f80015723249c0dccfc9969d50184ed76e7
Vitor Gabriel <Ranbut_Beater@protonmail.com>
2023-06-24 18:21:05 -0300

    feat: Added integration test delete of bookmarks route

--- tests/integration/bookmarks.test.ts
+++ tests/integration/bookmarks.test.ts
@@ -178,3 +178,49 @@ describe('POST /bookmarks', () => {
         });
     });
 });
+
+describe('DELETE /bookmarks/:id', () => {
+    it('should respond with status 401 if no token is given', async () => {
+        const response = await server.delete('/bookmarks/index');
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word();
+
+        const response = await server.delete('/bookmarks/index').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+        const response = await server.delete('/bookmarks/index').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe('when token is valid', () => {
+        it('should respond with status 404 when bookmark index not exist or not found', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+
+            const response = await server.delete("/bookmarks/index").set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.NOT_FOUND);
+        });
+
+        it('should respond with status 200', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            const bookmark = await createBookmark(user.id);
+
+            const response = await server.delete(`/bookmarks/${bookmark.index}`).set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.OK);
+        });
+    });
+});
