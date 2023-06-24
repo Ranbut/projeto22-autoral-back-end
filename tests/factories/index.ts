9c933c2b230cdeecdb44c002adf73a0c23894810
Vitor Gabriel <Ranbut_Beater@protonmail.com>
2023-06-24 17:28:33 -0300

    feat: Added integration test of equipments route

--- tests/factories/index.ts
+++ tests/factories/index.ts
@@ -2,3 +2,4 @@ export * from './users-factory';
 export * from './sessions-factory';
 export * from './monsters-factory';
 export * from './spells-factory';
+export * from './equipments-factory';
---
+++ tests/integration/equipments.test.ts
@@ -0,0 +1,280 @@
+import faker from '@faker-js/faker';
+import httpStatus from 'http-status';
+import * as jwt from 'jsonwebtoken';
+import supertest from 'supertest';
+import { cleanDb, generateValidToken } from '../helpers';
+import app, { init } from '@/app';
+import { createEquipment, createUser } from '../factories';
+
+beforeAll(async () => {
+    await init();
+});
+
+beforeEach(async () => {
+    await cleanDb();
+});
+
+const server = supertest(app);
+
+describe('GET /equipment', () => {
+    it('should respond with status 401 if no token is given', async () => {
+        const response = await server.get('/equipments');
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word();
+
+        const response = await server.get('/equipments').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+        const response = await server.get('/equipments').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe('when token is valid', () => {
+        it('should respond with status 200 and with equipments data', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            const equipment = await createEquipment(user.id);
+
+            const response = await server.get('/equipments').set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.OK);
+            expect(response.body).toEqual([
+                {
+                    id: equipment.id,
+                    userId: equipment.userId,
+                    equipment: equipment.equipment,
+                    createdAt: equipment.createdAt.toISOString(),
+                    updatedAt: equipment.updatedAt.toISOString(),
+                },
+            ]);
+        });
+    });
+});
+
+describe('GET /equipments/:id', () => {
+    it('should respond with status 401 if no token is given', async () => {
+        const response = await server.get('/equipments/1');
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word();
+
+        const response = await server.get('/equipments/1').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+        const response = await server.get('/equipments/1').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe('when token is valid', () => {
+        it('should respond with status 404 when equipments id not exist or not found', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+
+            const response = await server.get("/equipments/1").set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.NOT_FOUND);
+        });
+
+        it('should respond with status 200 and with equipments data', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            const equipment = await createEquipment(user.id);
+
+            const response = await server.get(`/equipments/${equipment.id}`).set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.OK);
+            expect(response.body).toEqual(
+                {
+                    id: equipment.id,
+                    userId: equipment.userId,
+                    equipment: equipment.equipment,
+                    createdAt: equipment.createdAt.toISOString(),
+                    updatedAt: equipment.updatedAt.toISOString(),
+                },
+            );
+        });
+    });
+});
+
+describe('POST /equipments', () => {
+    it('should respond with status 401 if no token is given', async () => {
+        const response = await server.post('/equipments');
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word();
+
+        const response = await server.post('/equipments').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+        const response = await server.post('/equipments').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe('when token is valid', () => {
+        it('should respond with status 400 when is body invalid', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+
+            const response = await server.post('/equipments').set('Authorization', `Bearer ${token}`).send({});
+
+            expect(response.status).toEqual(httpStatus.BAD_REQUEST);
+        });
+
+        it('should respond with status 201', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+
+            const equipmentBody = {
+                equipment: {
+                    name: faker.name.firstName()
+                }
+            };
+
+            const response = await server.post('/equipments').set('Authorization', `Bearer ${token}`).send(equipmentBody);
+
+            expect(response.status).toEqual(httpStatus.CREATED);
+            expect(response.body).toEqual({
+                id: expect.any(Number),
+                userId: expect.any(Number),
+                equipment: equipmentBody,
+                createdAt: expect.any(String),
+                updatedAt: expect.any(String)
+              });
+        });
+    });
+});
+
+describe('PUT /equipments/:id', () => {
+    it('should respond with status 401 if no token is given', async () => {
+        const response = await server.put('/equipments/1');
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word();
+
+        const response = await server.put('/equipments/1').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+        const response = await server.put('/equipments/1').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe('when token is valid', () => {
+        it('should respond with status 404 when equipment id not exist or not found', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+
+            const equipmentBody = {
+                equipment: {
+                    name: faker.name.firstName()
+                }
+            };
+
+            const response = await server.put("/equipments/1").set('Authorization', `Bearer ${token}`).send(equipmentBody);
+
+            expect(response.status).toEqual(httpStatus.NOT_FOUND);
+        });
+
+        it('should respond with status 200', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            const equipment = await createEquipment(user.id);
+
+            const equipmentBody = {
+                equipment: {
+                    name: faker.name.firstName()
+                }
+            };
+
+            const response = await server.put(`/equipments/${equipment.id}`).set('Authorization', `Bearer ${token}`).send(equipmentBody);
+
+            expect(response.status).toEqual(httpStatus.OK);
+        });
+    });
+});
+
+describe('DELETE /equipments/:id', () => {
+    it('should respond with status 401 if no token is given', async () => {
+        const response = await server.delete('/equipments/1');
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+        const token = faker.lorem.word();
+
+        const response = await server.delete('/equipments/1').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+        const userWithoutSession = await createUser();
+        const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+
+        const response = await server.delete('/equipments/1').set('Authorization', `Bearer ${token}`);
+
+        expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    describe('when token is valid', () => {
+        it('should respond with status 404 when equipment id not exist or not found', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+
+            const response = await server.delete("/equipments/1").set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.NOT_FOUND);
+        });
+
+        it('should respond with status 200', async () => {
+            const user = await createUser();
+            const token = await generateValidToken(user);
+            const equipment = await createEquipment(user.id);
+
+            const response = await server.delete(`/equipments/${equipment.id}`).set('Authorization', `Bearer ${token}`);
+
+            expect(response.status).toEqual(httpStatus.OK);
+        });
+    });
+});
